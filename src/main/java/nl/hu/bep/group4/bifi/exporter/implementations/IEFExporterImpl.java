9bc3c6899b38c076a27208aa3e7548dd1b95a263
Matthijs Osterhaus <matthijs@MacBook-Pro-van-Matthijs.local>
2019-06-22 21:20:42 +0200

    Export facturen werkt

--- src/main/java/nl/hu/bep/group4/bifi/exporter/implementations/IEFExporterImpl.java
+++ src/main/java/nl/hu/bep/group4/bifi/exporter/implementations/IEFExporterImpl.java
@@ -1,8 +1,12 @@
 package nl.hu.bep.group4.bifi.exporter.implementations;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import nl.hu.bep.group4.bifi.interfaces.IEFExporter;
 import nl.hu.bep.group4.bifi.model.Factuur;
@@ -15,32 +19,66 @@ import nl.hu.bep.group4.bifi.model.Persoon;
 public class IEFExporterImpl implements IEFExporter {
 
 	@Override
-	public void exportFacturen(List<Factuur> facturen) {
-		// TODO Auto-generated method stub
+	public void exportFacturen(List<Factuur> facturen){
+		int i = 0;
+		Map<Integer, String> factuurregels = new HashMap<>();
+		Writer file;
 		
+		StringBuilder IEFRegel = new StringBuilder();
+		IEFRegel.append("B" + exportChar("Factory Ansbach", 60) + exportChar("Eyber Strasse", 60)
+				+ exportChar("81", 10) + "9152BI" + exportChar("Ansbach", 20) + "DE81549090312"
+				+ exportChar("DE23DEU000198761111", 64) + "1234567890" + "\n");
+
+		for (Factuur factuur : facturen) {
+			IEFRegel.append(exportKlant(factuur.getKlant(), factuur.getContactPersoon()) + "\n");
+			IEFRegel.append(invoiceInformatieRegel(factuur) + "\n");
+			for (FactuurRegel regel : factuur.getFactuurregels()) {
+				IEFRegel.append(exportFactuurRegel(regel, factuur) + "\n");
+				IEFRegel.append(exportTekstRegel(regel.getProductNaam()));
+			}
+			factuurregels.put(i, IEFRegel.toString());
+			i += 1;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (String regel : factuurregels.values()) {
+			sb.append(regel);
+		}
+		
+		try {
+			String slash = File.separator;
+			String property = System.getProperty("user.home");
+			String fileName ="createdFile.txt";
+			file = new FileWriter(property+slash+"AppName"+slash+fileName);
+			file.write(sb.toString());
+			file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public String exportChar(String value, int length) {
 		String result = value;
-		if(result.length() > length) {
+		if (result.length() > length) {
 			result = result.substring(0, length);
 		}
-		for(int i=0;i<length-value.length();i++) {
+		for (int i = 0; i < length - value.length(); i++) {
 			result += " ";
 		}
 		return result;
 	}
 
 	public String exportDouble(double value, int beforeComma, int afterComma) {
-		String result = String.format("%"+beforeComma+"."+afterComma+"f", value).replaceAll(",", "").replaceAll("\\.", "").replaceAll("-", "");
-		if(result.length() > beforeComma+afterComma) {
-			result = result.substring(result.length()-(beforeComma+afterComma));
+		String result = String.format("%" + beforeComma + "." + afterComma + "f", value).replaceAll(",", "")
+				.replaceAll("\\.", "").replaceAll("-", "");
+		if (result.length() > beforeComma + afterComma) {
+			result = result.substring(result.length() - (beforeComma + afterComma));
 		}
-		int zeros = beforeComma+afterComma-result.length();
-		for(int i=0;i<zeros;i++) {
-			result = "0"+result;
+		int zeros = beforeComma + afterComma - result.length();
+		for (int i = 0; i < zeros; i++) {
+			result = "0" + result;
 		}
-		if(value < 0) {
+		if (value < 0) {
 			result = result.replaceAll("0", " ");
 			result = result.replaceAll("1", "!");
 			result = result.replaceAll("2", "\"");
@@ -56,69 +94,80 @@ public class IEFExporterImpl implements IEFExporter {
 	}
 
 	public String exportKlant(Klant klant, Persoon persoon) {
-		return "K"
-				+exportChar(klant.getBedrijfsnaam(), 40)
-				+exportChar(getAanhef(persoon), 6)
-				+exportChar(persoon.getVoornaam(), 20)
-				+exportChar(persoon.getTussenvoegsel(), 7)
-				+exportChar(persoon.getAchternaam(), 40)
-				+exportChar(klant.getFactuurAdres().getStraat(), 60)
-				+exportChar(klant.getFactuurAdres().getHuisnummer(), 10)
-				+exportChar(klant.getFactuurAdres().getPostcode(), 6)
-				+exportChar(klant.getFactuurAdres().getPlaats(), 20)
-				+exportChar(klant.getVAT(), 13)
-				+exportChar(klant.getBankrekeningNummer(), 64)
-				+exportChar(klant.getBiC(), 10)
-				;
+		return "K" + exportChar(klant.getBedrijfsnaam(), 40) + exportChar(getAanhef(persoon), 6)
+				+ exportChar(persoon.getVoornaam(), 20) + exportChar(persoon.getTussenvoegsel(), 7)
+				+ exportChar(persoon.getAchternaam(), 40) + exportChar(klant.getFactuurAdres().getStraat(), 60)
+				+ exportChar(klant.getFactuurAdres().getHuisnummer(), 10)
+				+ exportChar(klant.getFactuurAdres().getPostcode(), 6)
+				+ exportChar(klant.getFactuurAdres().getPlaats(), 20) + exportChar(klant.getVAT(), 13)
+				+ exportChar(klant.getBankrekeningNummer(), 64) + exportChar(klant.getBiC(), 10);
 	}
-	
+
 	private String getAanhef(Persoon persoon) {
-		if(persoon.getGeslacht() == Persoon.Geslacht.MAN) {
+		if (persoon.getGeslacht() == Persoon.Geslacht.MAN) {
 			return "Dhr.";
 		} else {
 			return "Mvr.";
 		}
 	}
+	
+	public String exportDate(String datumTijd) {
+		String[] dateParts = datumTijd.split("-");
+		return dateParts[2].split("T")[0] + dateParts[1] + dateParts[0].substring(2,4);
+	}
+	
+	public String exportTijd(String datumTijd) {
+		String factuurTijd = datumTijd.split("T")[1];
+		return factuurTijd.substring(0,2) + factuurTijd.substring(3,5);
+	}
 
-	public String exportFactuurRegel(FactuurRegel factuurRegel) {
-		return "R"
-				+exportChar(factuurRegel.getProductNaam(), 60)
-				+exportDouble(factuurRegel.getAantal(), 3, 2)
-				+exportDouble(factuurRegel.getTotaalprijsExBTW()/factuurRegel.getAantal(), 5, 2)
-				+exportBtwType(factuurRegel.getBtwCode())
-				//TODO: datum
-				+exportChar(convertUnit(factuurRegel.getUnit()), 6)
-				;
+	public String exportFactuurRegel(FactuurRegel factuurRegel, Factuur factuur) {
+		return "R" + exportChar(factuurRegel.getProductNaam(), 60) + exportDouble(factuurRegel.getAantal(), 3, 2)
+				+ exportDouble(factuurRegel.getTotaalprijsExBTW() / factuurRegel.getAantal(), 5, 2)
+				+ exportBtwType(factuurRegel.getBtwCode()) 
+				+ exportDate(factuur.getDatumtijd())
+				+ exportTijd(factuur.getDatumtijd())
+				+ exportChar(convertUnit(factuurRegel.getUnit()), 6);
+	}
+	
+	public String exportTekstRegel(String productOmschrijving) {
+		if(productOmschrijving.length() >= 60) {
+			productOmschrijving = "T" + exportChar(productOmschrijving.substring(60, productOmschrijving.length()), 120);
+		}else {
+			productOmschrijving = "";
+		}
+		return productOmschrijving;
 	}
 	
 	public String invoiceInformatieRegel(Factuur factuur) {
-		try {
-			return "F"
-					+new SimpleDateFormat("ddMMyyHHmm").format(new SimpleDateFormat().parse(factuur.getDatumtijd()))
-					;
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-	}	
+		String factuurNummer = Integer.toString(factuur.getFactuurNummer());
+		String date = exportDate(factuur.getDatumtijd());
+		return "F" + date + exportChar(factuurNummer, 10);
+	}
 
 	private String convertUnit(Unit unit) {
-		if(unit == Unit.KILOGRAM) {
+		if (unit == Unit.KILOGRAM) {
 			return "kg";
 		}
 		return null;
 	}
 
 	private String exportBtwType(BTWcode btwCode) {
-		switch(btwCode) {
-			case GEEN:
-				return "0";
-			case LAAG:
-				return "2";
-			case HOOG:
-				return "3";
+		switch (btwCode) {
+		case GEEN:
+			return "0";
+		case LAAG:
+			return "2";
+		case HOOG:
+			return "3";
 		}
 		return null;
+	}
+
+	public String exportBedrijfsInformatie(String bedrijfsnaam, String straat, String huisnummer, String postcode,
+			String plaats, String btw, String iban, String bic) {
+		return "B" + exportChar(bedrijfsnaam, 60) + exportChar(straat, 60) + exportChar(huisnummer, 10)
+				+ exportChar(postcode, 6) + exportChar(plaats, 20) + exportChar(btw, 13) + exportChar(iban, 64)
+				+ exportChar(bic, 60);
 	}
 }
--- src/test/java/nl/hu/bep/group4/bifi/exporter/IEFExporterTest.java
+++ src/test/java/nl/hu/bep/group4/bifi/exporter/IEFExporterTest.java
@@ -1,7 +1,6 @@
 package nl.hu.bep.group4.bifi.exporter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import nl.hu.bep.group4.bifi.model.Adres;
 import nl.hu.bep.group4.bifi.model.Factuur;
 import nl.hu.bep.group4.bifi.model.FactuurRegel;
@@ -82,6 +81,18 @@ public class IEFExporterTest {
 	}
 	
 	@Test
+	public void testExportDate() {
+		IEFExporterImpl exporter = new IEFExporterImpl();
+		assertEquals("031214", exporter.exportDate("2014-12-03T10:15:30.00Z"));
+	}
+	
+	@Test
+	public void testExportTijd() {
+		IEFExporterImpl exporter = new IEFExporterImpl();
+		assertEquals("1015", exporter.exportTijd("2014-12-03T10:15:30.00Z"));
+	}
+	
+	@Test
 	public void testExportKlant() {
 		IEFExporterImpl exporter = new IEFExporterImpl();
 		Adres adres = new Adres("nepstraat", "666", "3582XN", "Hell", "1234");
@@ -94,10 +105,25 @@ public class IEFExporterTest {
 	@Test
 	public void testExportFactuurRegel() {
 		IEFExporterImpl exporter = new IEFExporterImpl();
-		FactuurRegel factuurRegel = new FactuurRegel(5,"Bifi betstelauto van worst", 30, 96000, BTWcode.LAAG, Unit.KILOGRAM);
+		Factuur factuur = new Factuur();
+		factuur.setDatumtijd("2014-12-03T10:15:30.00Z");
+		FactuurRegel factuurRegel = new FactuurRegel(5,"Bifi bestelauto van worst", 30, 96000, BTWcode.LAAG, Unit.KILOGRAM);
 		
-
-		assertEquals("RBifi auto van worst                                         0300003200002110818kg    ", exporter.exportFactuurRegel(factuurRegel));
+		assertEquals("RBifi bestelauto van worst                                   03000032000020312141015kg    ", exporter.exportFactuurRegel(factuurRegel, factuur));
+	}
+	
+	@Test
+	public void testExportTekstRegel() {
+		IEFExporterImpl exporter = new IEFExporterImpl();
+		String productOmschrijving = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet vulputate quam, eu viverra nisi. Quisque posuere eu urna vel posuere cras amet.";
+		
+		assertEquals("Ts sit amet vulputate quam, eu viverra nisi. Quisque posuere eu urna vel posuere cras amet.                              ", exporter.exportTekstRegel(productOmschrijving));
+	}
+	
+	@Test
+	public void testExportTekstRegelKleinerDanZestig() {
+		IEFExporterImpl exporter = new IEFExporterImpl();
+		assertEquals("", exporter.exportTekstRegel("Dit is een omschrijving"));
 	}
 
 	@Test
@@ -115,6 +141,58 @@ public class IEFExporterTest {
 
 
 		Factuur factuur = new Factuur(klant, "2014-12-03T10:15:30.00Z", 1, factuurregels, "Opmerking", persoon);
-		assertEquals("F2014-12-03T10:15:30.00Z         ", exporter.invoiceInformatieRegel(factuur));
+		assertEquals("F0312141         ", exporter.invoiceInformatieRegel(factuur));
+	}
+	
+	@Test
+	public void testExportFacturen() {
+		IEFExporterImpl exporter = new IEFExporterImpl();
+		List<Factuur> facturen = new ArrayList<>();
+		
+		List<FactuurRegel> factuurregels = new ArrayList<>();
+		Adres adres = new Adres("nepstraat", "666", "3582XN", "Hell", "1234");
+		Klant klant = new Klant(5, "Testbedrijf", "bv", "testVat", "testRekening", "testGiroNummer", "testBic", null, null, adres);
+		FactuurRegel factuurRegel = new FactuurRegel(5,"Bifi bestelauto van worst", 30, 96000, BTWcode.LAAG, Unit.KILOGRAM);
+		Persoon persoon = new Persoon(2, "Matthias", "Judas", "tussen", "0609090906", "nee", Persoon.Geslacht.MAN);
+		factuurregels.add(factuurRegel);
+		Factuur factuur = new Factuur(klant, "2014-12-03T10:15:30.00Z", 1, factuurregels, "String", persoon);
+		facturen.add(factuur);
+		
+		exporter.exportFacturen(facturen);
+		
+//		assertEquals("BFactory Ansbach                                             Eyber Strasse                                               81        9152BIAnsbach             DE81549090312DE23DEU000198761111                                             1234567890\nKTestbedrijf                             Dhr.  Matthias            tussen Judas                                   nepstraat                                                   666       3582XNHell                testVat      testRekening                                                    testBic   \nF0312141         \nRBifi bestelauto van worst                                   03000032000020312141015kg    \n", exporter.exportFacturen(facturen));
+
+	}
+	
+	@Test
+	public void testExportTweeFacturen() {
+		IEFExporterImpl exporter = new IEFExporterImpl();
+		List<Factuur> facturen = new ArrayList<>();
+		List<FactuurRegel> factuurregels = new ArrayList<>();
+		
+		Adres adres = new Adres("nepstraat", "666", "3582XN", "Hell", "1234");
+		Klant klant = new Klant(5, "Testbedrijf", "bv", "testVat", "testRekening", "testGiroNummer", "testBic", null, null, adres);
+		FactuurRegel factuurRegel = new FactuurRegel(5,"Bifi bestelauto van worst", 30, 96000, BTWcode.LAAG, Unit.KILOGRAM);
+		FactuurRegel factuurRegel2 = new FactuurRegel(6,"Bifi worst", 300, 15011, BTWcode.LAAG, Unit.KILOGRAM);
+		Persoon persoon = new Persoon(2, "Matthias", "Judas", "tussen", "0609090906", "nee", Persoon.Geslacht.MAN);
+		factuurregels.add(factuurRegel);
+		factuurregels.add(factuurRegel2);
+		Factuur factuur = new Factuur(klant, "2014-12-03T10:15:30.00Z", 1, factuurregels, "String", persoon);
+		facturen.add(factuur);
+		
+		exporter.exportFacturen(facturen);
+		
+		assertEquals("BFactory Ansbach                                             Eyber Strasse                                               81        9152BIAnsbach             DE81549090312DE23DEU000198761111                                             1234567890\n" + 
+				"KTestbedrijf                             Dhr.  Matthias            tussen Judas                                   nepstraat                                                   666       3582XNHell                testVat      testRekening                                                    testBic   \n" + 
+				"F0312141         \n" + 
+				"RBifi bestelauto van worst                                   03000032000020312141015kg    \n" + 
+				"RBifi worst                                                  30000000500420312141015kg   \n", exporter.exportFacturen(facturen));
+
+	}
+	
+	@Test
+	public void testExportBedrijfsInformatie() {
+		IEFExporterImpl exporter = new IEFExporterImpl();
+		assertEquals("BTestnaam                                                    teststraat                                                  123       1234ABtestplaats          BTW          INGB03475                                                       BIC                                                         ", exporter.exportBedrijfsInformatie("Testnaam", "teststraat", "123", "1234AB", "testplaats", "BTW", "INGB03475", "BIC"));
 	}
 }
